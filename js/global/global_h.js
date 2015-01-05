b25ad6b6178d19af18ed8a1202f618e7b369d8fe
leico <yoshitaka_12@iamas.ac.jp>
2015-01-05 10:37:59 +0900

    add Page setting

--- js/global/global_h.js
+++ js/global/global_h.js
@@ -2,93 +2,99 @@
 
 if( ! $().jquery) alert('Require jQuery');
 
-var $zen = {    //namespace $zen;
-	$global : {} //namespace $zen :: $global;
+var $leico = {    //namespace $leico;
+  $global : {} //namespace $leico :: $global;
 };
 
-((function(undefined)
-{
-	/* ==================================== *
-	 * namespace(global, property)          *
-	 * ------------------------------------ *
-	 * 名前空間にオブジェクトを追加する．   *
-	 * global    : グローバルな名前空間     *
-	 * namespace : 追加する名前空間         *
-	 * property  : 追加するオブジェクト     *
-	 * ==================================== */
-	$zen.$global.namespace = function(global, namespace, property, undefined)
-	{
-		global            = global            || {};
-		global[namespace] = global[namespace] || {};
-		property          = property          || {};
+((function(undefined){
 
-		Object.keys(property).forEach(function(key)
-		{
-			global[namespace][key] = property[key];
-		});
-	};
-})());
+  var $ns = $leico.$global;
 
-((function(undefined)
-{
-	/* ======================================== *
-	 * include(filename)                        *
-	 * ---------------------------------------- *
-	 * 他Javascriptファイルの読み込みを行う     *
-	 * filename : ファイル名                    *
-	 * ======================================== */
-	$zen.$global.include = function(filename)
-	{
-		$('head').append('<script type="text/javascript" src="' + filename + '"></script>');
-	}
+  ((function(undefined)
+    {
+      /* ==================================== *
+       * namespace(global, property)          *
+       * ------------------------------------ *
+       * 名前空間にオブジェクトを追加する．   *
+       * global    : グローバルな名前空間     *
+       * namespace : 追加する名前空間         *
+       * property  : 追加するオブジェクト     *
+       * ==================================== */
+      $ns.namespace = function(global, namespace, property, undefined)
+      {
+        global            = global            || {};
+        global[namespace] = global[namespace] || {};
+        property          = property          || {};
 
-})());
+        Object.keys(property).forEach(function(key)
+            {
+              global[namespace][key] = property[key];
+            });
+      };
+    })());
 
+  ((function(undefined)
+    {
+      /* ======================================== *
+       * include(filename)                        *
+       * ---------------------------------------- *
+       * 他Javascriptファイルの読み込みを行う     *
+       * filename : ファイル名                    *
+       * ======================================== */
+      $ns.include = function(filename)
+      {
+        $('head').append('<script type="text/javascript" src="' + filename + '"></script>');
+      }
 
-((function(undefined)
-{
-	/* ========================================= *
-	 * inheritance(child, parent)                *
-	 * ----------------------------------------- *
-	 * 継承を行う                                *
-	 * child  : 子クラス                         *
-	 * parent : 親クラス                         *
-	 * ========================================= */
-	$zen.$global.inheritance = function(child, parent)
-	{
-		child.prototype             = Object.create(parent.prototype);
-		child.prototype.constructor = child;
-		child.super                 = parent.prototype;
-	}
-})());
+    })());
 
 
-((function(undefined)
-{
-	/* =============================================== *
-	 * extend(parent, property)                        *
-	 * ----------------------------------------------- *
-	 * parentを継承した子クラスを作成する．            *
-	 * parent   : 親クラス                             *
-	 * property : 子クラスのprototypeメソッド          *
-	 * =============================================== */
-	$zen.$global.extend = function(parent, property)
-	{
-		parent   = parent   || Object; //parent   is null, parent   is Object
-		property = property || {}; //property is null, property is Object
+  ((function(undefined)
+    {
+      /* ========================================= *
+       * inheritance(child, parent)                *
+       * ----------------------------------------- *
+       * 継承を行う                                *
+       * child  : 子クラス                         *
+       * parent : 親クラス                         *
+       * ========================================= */
+      $ns.inheritance = function(child, parent)
+      {
+        child.prototype             = Object.create(parent.prototype);
+        child.prototype.constructor = child;
+        child.super                 = parent.prototype;
+      }
+    })());
 
-		//if property do not have constructor, use parent constructor
-		var child = property.constructor || function(){ parent.apply(this, arguments); };
 
-		$zen.$global.inheritance(child, parent); //INHERITANCE
+  ((function(undefined)
+    {
+      /* =============================================== *
+       * extend(parent, property)                        *
+       * ----------------------------------------------- *
+       * parentを継承した子クラスを作成する．            *
+       * parent   : 親クラス                             *
+       * property : 子クラスのprototypeメソッド          *
+       * =============================================== */
+      $ns.extend = function(parent, property)
+      {
+        parent   = parent   || Object; //parent   is null, parent   is Object
+        property = property || {}; //property is null, property is Object
 
-		//make List 
-		Object.keys(property).forEach(function(key)
-		{
-			if(key !== 'constructor') child.prototype[key] = property[key];
-		});
+        //if property do not have constructor, use parent constructor
+        var child = property.constructor || function(){ parent.apply(this, arguments); };
 
-		return child;
-	}
+        $zen.$global.inheritance(child, parent); //INHERITANCE
+
+        //make List 
+        Object.keys(property).forEach(function(key)
+            {
+              if(key !== 'constructor') child.prototype[key] = property[key];
+            });
+
+        return child;
+      }
+    })());
+
 })());
 
--- js/main.js
+++ js/main.js
@@ -1,44 +1,312 @@
-
+/* ==================================== *
+ * background setting                   *
+ * ==================================== */
 ((function(undefined){
-
-  var bg = ((function(undefined){
-    var wide = $(window).width();
-    return './image/background/' + ( (wide > 2560) ? 5120 : (wide > 1920) ? 2560 : 1920 ) + '.jpg';
-  })());
-
-  $('#bg').hide().css({ backgroundImage : 'url("' + bg + '")' });
-
-  $('div.icon>h3').hide();
+  
+  var $ns = $leico.$global;
+  var bg_table = ['wall', 'netwall'];
+  $ns.bg = './image/' + bg_table[ (Math.random() > 0.5) ? 1 : 0 ] + '/';
 
 })());
 
 
+/* ==================================== *
+ * window resize setting                *
+ * ==================================== */
 ((function(undefined){
 
-  $('div.icon').hover(function(){
-    $('#bg')
+  $(window).resize(function(){
+    var $ns = $leico.$global;
+
+    $ns.width  = $(window).width();
+    $ns.height = $(window).height();
+
+    var focus = $('#top_icon');
+
+    var fh_width  = focus.width()  / 2;
+    var fh_height = focus.height() / 2;
+    var h_width   = $ns.width      / 2;
+    var h_height  = $ns.height     / 2;
+
+    focus.css({
+      position : 'absolute'
+        , top    : h_height - fh_height
+        , left   : h_width  - fh_width
+        , right  : h_width  - fh_width
+        , bottom : h_height - fh_height
+    });
+  })
+  .trigger('resize');
+
+})());
+
+
+/* ==================================== *
+ * choose background size               *
+ * ==================================== */
+((function(undefined){
+
+  var $ns = $leico.$global;
+
+  var bg = $ns.bg + chooseImageSize() + '.jpg';
+  $('#bg').append(
+      $('<div>')
+      .hide()
+      .addClass('center')
+      .css({
+          width  : '100%'
+        , height : '100%'
+        , backgroundImage    : 'url("' + bg + '")'
+        , backgroundPosition : 'center center'
+        , backgroundSize     : 'cover'
+      })
+  );
+
+  $('#top_icon>h3').hide();
+
+})());
+
+
+
+/* ==================================== *
+ * Top Icon & background Event register *
+ * ==================================== */
+((function(undefined){
+
+  $('#top_icon').hover(function(){
+    $('#bg>div')
       .stop()
-      .fadeIn(
-          25000
-        , function(){
-            $('div.icon')
-              .delay(5000)
-              .queue(function(){ $(this).trigger('click').dequeue(); });
-        }
-      );
-    $('div.icon>h3').fadeIn('fast');
+      .fadeIn(25000, function(){
+        $(this)
+          .delay(5000)
+          .queue(function(){ $('#top_icon').trigger('click').dequeue(); });
+      });
+    $('#top_icon>h3').stop().fadeIn('fast');
   }
   , function(){
-    $('#bg').stop().fadeOut(300);
-    $('div.icon>h3').fadeOut(500);
+    $('#bg>div')     .stop().fadeOut(300);
+    $('#top_icon>h3').stop().fadeOut(500);
   })
   .click(function(e){
-    $('div.icon')
-      .unbind('mouseenter')
-      .unbind('mouseleave')
-      .fadeOut('fast');
 
-    $('#bg').stop();
+    var $ns  = $leico.$global;
+    var self = this;
+
+    //Top animation and unbind All Event
+    ((function(){
+      $(self)
+        .attr('id', 'leico')
+        .unbind('click')
+        .unbind('mouseenter')
+        .unbind('mouseleave')
+        .animate(
+            {top  : '20%'}
+          , {
+                duration :  600
+              , complete : function(){ $('body').trigger('contents'); }
+          }
+        );
+
+      $(window).unbind('resize');
+
+      $('#bg>div').stop().fadeOut('fast', function(){ $(this).remove(); });
+    })());
+
+    //get my works
+    $ns.works = ((function(){
+      var works;
+
+      $.ajax({
+          url           : './data/data.json'
+        , async         : false
+        , cache         : false
+        , scriptCharset : 'utf-8'
+        , dataType      : 'json'
+        , success       : function(data){ works = data; }
+        , error         : function(XMLHttpRequest, textStatus, errorThrown){
+          console.log('could not get data');
+          console.log(textStatus);
+        }
+      });
+
+      return works;
+    })());
+
   });
 
 })());
+
+
+
+/* ==================================== *
+ * go to contents, regist contents      *
+ * ==================================== */
+((function(){
+
+  var $ns = $leico.$global;
+
+  $('body').bind('contents', function(){
+
+    $('#leico')
+    .queue(function(){
+
+      var sel = $(this);
+      var i = 0;
+
+      sel.data('index', i).click( iconClick );
+
+      ++ i;
+
+      for( i ; i < $ns.works.length ; ++ i){
+
+        sel.parent().prepend(
+          $('<div>')
+          .addClass('icon')
+          .data('index', i)
+          .append(
+            $('<h3>').text( $ns.works[i].title )
+          )
+          .attr('id', $ns.works[i].title)
+          .css({
+              position : sel.css('position')
+            , top      : sel.offset().top
+            , left     : sel.offset().left
+          })
+          .click( iconClick )
+        );
+
+      }
+
+      sel.dequeue();
+    })
+    .delay(500)
+    .queue(function(){
+
+      $('div.icon').each(function(index, element){
+        $(this).animate({left : (30 * (index + 1)) + '%'}, {duration : 1000});
+      })
+
+      $(this).dequeue();
+    })
+
+    
+  });
+
+  /* ========================================= *
+   * iconClick(event)                          *
+   * ========================================= */
+  function iconClick(){
+
+    var $ns = $leico.$global;
+
+    var data = $ns.works[$(this).data('index')];
+    switch(data.type){
+      case 'image': setImage( $(this), data ); break;
+      case 'page' : setPage ( $(this), data ); break;
+    }
+
+    $ns.scroll = $('#contents').scrollLeft();
+
+    $('#contents').fadeOut(2000);
+
+  }
+
+  /* ========================================= *
+   * setDescription(selector, data, position)  *
+   * ========================================= */
+  function setDescription(sel, data, offset){
+
+  }
+/* ==================================== *
+ * setPage(selector, data)              *
+ * ==================================== */
+  function setPage(sel, data){
+
+
+    if(data.contents == null) return;
+
+    var $ns = $leico.$global;
+
+    $('#bg').append(
+        $('<iframe>').attr({
+            src         : data.contents
+          , frameborder : '1'
+        })
+        .addClass('center')
+        .css({
+            width  : '80%'
+          , height : '80%'
+        })
+        .hide()
+        .stop()
+        .fadeIn(5000)
+    )
+    .click(function(){
+
+      $(this).unbind('click')
+        .children('iframe')
+          .stop()
+          .fadeOut(2000, function(){ $(this).remove(); })
+        .end();
+
+      $('#contents')
+        .scrollLeft($ns.scroll)
+        .fadeIn('fast', function(){});
+
+    });
+    
+  }
+
+
+/* ==================================== *
+ * setImage(selector, data)             *
+ * ==================================== */
+  function setImage(sel, data){
+
+    if(data.contents == null) return;
+
+    var $ns = $leico.$global;
+
+    var bg =  data.contents + '/' + chooseImageSize() + '.' + data.imagetype;
+
+    $('#bg').append(
+      $('<div>')
+        .addClass('center')
+        .css({
+            backgroundImage :  'url("' + bg + '")'
+          , backgroundPosition : 'center center'
+          , backgroundSize     : 'cover'
+          , width  : '100%'
+          , height : '100%'
+        })
+        .hide()
+        .stop()
+        .fadeIn(5000)
+    )
+    .click(function(){
+      $(this)
+        .unbind('click')
+        .children('div')
+          .stop()
+          .fadeOut(2000, function(){ $(this).remove(); })
+        .end();
+
+      $('#contents')
+        .scrollLeft($ns.scroll)
+        .fadeIn('fast', function(){});
+    })
+  }
+})());
+
+
+
+/* ==================================== *
+ * chooseImageSize                      *
+ * ==================================== */
+function chooseImageSize(){
+  var $ns = $leico.$global;
+  var width = $ns.width;
+
+  return ( (width > 2560) ? 5120 : (width > 1920) ? 2560 : 1920);
+}
+
